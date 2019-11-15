0486c113191a04ed55045b5e64594ed4cdda04aa
Mikołaj Kaźmierczak <mikolaj@kazmierczak.pl>
2019-11-15 05:35:50 +0100

    Sprawne menu

--- layout/menu.php
+++ layout/menu.php
@@ -3,4 +3,28 @@
     <img src="<?php echo $rootUrl . 'src/img/logo_white.svg' ?>">
     <h1>PWr Diving Crew</h1>
   </div>
+  <ul>
+    <?php
+
+    function menu_item($page, $title) {
+      global $rootUrl;
+      $full_url = $rootUrl."pages/".$page.".php";
+      echo
+        '<li>
+            <a href="'.$full_url.'">'.$title.'</a>
+         </li>';
+    }
+
+    menu_item("404", "Wizja");
+    menu_item("404", "Projekty");
+    menu_item("404", "PWr Diving Crew");
+    menu_item("404", "Załoga");
+    menu_item("404", "Dziedziny");
+    menu_item("404", "Kontakt");
+    menu_item("404", "Partnerzy");
+
+    ?>
+    <li id="pl"><img src="<?php echo $rootUrl . 'src/img/pl.svg' ?>"></li>
+    <li id="en"><img src="<?php echo $rootUrl . 'src/img/en.svg' ?>"></li>
+  </ul>
 </menu>
